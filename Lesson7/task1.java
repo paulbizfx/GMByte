39a758e496280f9e7123903c297c12095c230a0a
FireDemon <vladlen2010@mail.ru>
2015-05-27 01:51:18 +0300

    static method

---
+++ Lesson7/task1.java
@@ -0,0 +1,15 @@
+package Lesson7;
+
+public class task1 {
+    public static void main(String[] args) {
+        byte a = 5;
+        method(a);
+
+
+    }
+    static void method(int a) {
+
+        System.out.println("В переменной a хранится число " + a);
+    }
+
+}

7a5292231bad872c915920f6f51bf4dfce9ff20d
FireDemon <vladlen2010@mail.ru>
2015-05-27 00:23:51 +0300

    peremennie

---
+++ Lesson3/task1.java
@@ -0,0 +1,16 @@
+package Lesson3;
+
+
+public class task1 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 10;
+        System.out.println("Первая переменная хранит значение " + a);
+        System.out.println("Вторая переменная хранит значение " + b);
+        System.out.println("Результат сложения = " + (a + b) );
+        System.out.println("Результат вычитания = " + (a - b));
+        System.out.println("Результат умножения = " + (a * b));
+        System.out.println("Результат деления = " + (a / b));
+
+    }
+}

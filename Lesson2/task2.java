416e427169c01d73103611bed68fbe13739d65a4
FireDemon <vladlen2010@mail.ru>
2015-05-26 23:59:30 +0300

    first commit

---
+++ Lesson2/task2.java
@@ -0,0 +1,11 @@
+package Lesson1;
+
+public class task2 {
+    public static void main(String[] args) {
+        System.out.print("Java");
+        System.out.print(" " + "Java");
+        System.out.print(" " + "Java");
+        System.out.print(" " + "Java");
+        System.out.print(" " + "Java");
+    }
+}

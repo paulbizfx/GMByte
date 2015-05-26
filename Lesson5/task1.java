80895e63e687b35b45d9fa2cd6df56c33535ede1
FireDemon <vladlen2010@mail.ru>
2015-05-27 00:39:05 +0300

    methods

---
+++ Lesson5/task1.java
@@ -0,0 +1,19 @@
+package Lesson5;
+
+public class task1 {
+    public static void main(String[] args) {
+        Vanya();
+        Alex();
+
+    }
+
+
+
+    static void Vanya()    {
+        System.out.println("Я - Vanya. Звали?");
+    }
+
+    static void Alex() {
+        System.out.println("Я - Alex. Звали?");
+    }
+}

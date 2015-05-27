6de419e20ab85aa51c1af64f5d6b1c0bf6b832db
FireDemon <vladlen2010@mail.ru>
2015-05-27 21:49:17 +0300

    Private, public, defaut

---
+++ Lesson9/classs/ss.java
@@ -0,0 +1,21 @@
+package Lesson9.classs;
+
+public class ss {
+    public int a = 1;
+    private int b = 2;
+    int c = 2;
+
+    public void a() {
+        System.out.println("public");
+
+    }
+
+    private void b() {
+        System.out.println("private");
+    }
+
+    void c() {
+
+        System.out.println("default");
+    }
+}
---
+++ Lesson9/task1.java
@@ -0,0 +1,20 @@
+package Lesson9;
+
+import Lesson9.classs.ss;
+
+public class task1 {
+    public static void main(String[] args) {
+        ss ss = new ss();
+        ss.a();
+        int aa = ss.a;
+
+
+        ss.b();
+        int bb = ss.b;
+
+        ss.c();
+        int cc = ss.c;
+
+
+    }
+}

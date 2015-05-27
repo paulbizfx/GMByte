adc81e1b12b31d34f2e3b475a4191dbe42aa8fb0
FireDemon <vladlen2010@mail.ru>
2015-05-27 20:08:07 +0300

    args method

---
+++ Lesson8/task1.java
@@ -0,0 +1,12 @@
+package Lesson8;
+
+public class task1 {
+    public static void main(String[] args) {
+        byte a = 1;
+        method(a);
+    }
+
+    static void method(byte q) {
+        System.out.println("В качестве аргументов мне передали " + q);
+    }
+}

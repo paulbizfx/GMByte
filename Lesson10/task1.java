1bfaa5a154f195d640c3320355cae41c134416a4
FireDemon <vladlen2010@mail.ru>
2015-05-28 20:11:40 +0300

    constructor

---
+++ Lesson10/task1.java
@@ -0,0 +1,17 @@
+package Lesson10;
+
+public class task1 {
+    public static void main(String[] args) {
+        task1 task1 = new task1(1,2,3,4);
+    }
+         int a, b, c, d;
+
+      task1(int a, int b, int c, int d) {
+         this.a = a;
+         this.b = b;
+         this.c = c;
+         this.d = d;
+
+         System.out.println("Меня создали!");
+     }
+}

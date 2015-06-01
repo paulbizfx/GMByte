deb0dde788bceb5df45c9c82d7b28f0e41d695dc
FireDemon <vladlen2010@mail.ru>
2015-06-01 20:54:33 +0300

    static

--- Lesson7/task1.java
+++ Lesson7/task1.java
@@ -3,11 +3,7 @@ package Lesson7;
 public class task1 {
     byte a = 5;
     public static void main(String[] args) {
-
-
         method();
-
-
     }
     static void method() {
         task1 task1 = new task1();

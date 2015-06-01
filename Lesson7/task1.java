f2b965220e6d4e7707c098a3ad55947d197b63e5
FireDemon <vladlen2010@mail.ru>
2015-06-01 20:51:35 +0300

    method

--- Lesson7/task1.java
+++ Lesson7/task1.java
@@ -1,16 +1,17 @@
 package Lesson7;
 
 public class task1 {
+    byte a = 5;
     public static void main(String[] args) {
 
-        byte a = 5;
-        method(a);
+
+        method();
 
 
     }
-    static void method(int a) {
-
-        System.out.println("В переменной a хранится число " + a);
+    static void method() {
+        task1 task1 = new task1();
+        System.out.println("В переменной a хранится число " + task1.a);
     }
 
 }

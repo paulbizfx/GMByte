a1d950d57f5bcb75ecf2661f7f60f6e5bc4cf6e8
FireDemon <vladlen2010@mail.ru>
2015-06-02 14:56:57 +0300

    Switch

---
+++ Lesson12/task2.java
@@ -0,0 +1,18 @@
+package Lesson12;
+
+public class task2 {
+    public static void main(String[] args) {
+        byte a = 1;
+        switch (a) {
+            case 1:
+                System.out.println("a = 1");
+                break;
+            case 2:
+                System.out.println("a = 2");
+                break;
+            default:
+                System.out.println("Я такого числа не знаю");
+                break;
+        }
+    }
+}

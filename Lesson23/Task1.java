8024a01cf56891483d11682af4870495de0a0523
FireDemon <vladlen2010@mail.ru>
2015-06-15 01:18:58 +0300

    Equals

---
+++ Lesson23/Task1.java
@@ -0,0 +1,17 @@
+package Lesson23;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String str = "String";
+        String str1 = "String";
+        if (str.equals(str1)) System.out.println(true);
+        else System.out.println(false);
+
+
+        String string = new String("String");
+        String string1 = new String("String");
+        if (string == string1) System.out.println(true);
+        else System.out.println(false);
+
+    }
+}

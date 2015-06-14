a8bc5b1351838ed308a6fab986ebfbdffee8fc41
FireDemon <vladlen2010@mail.ru>
2015-06-14 23:46:56 +0300

    String

---
+++ Lesson22/Task1.java
@@ -0,0 +1,11 @@
+package Lesson22;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String s = "Теперь я умею создавать и хранить строки";
+        System.out.print(s + ". ");
+        byte q = 10;
+        String s1 = "Я даже прикрепил переменную к этой строке, её значение: ";
+        System.out.println(s1 + q);
+    }
+}

ba1341f2d834bec10de6178c322558eaba0d027a
FireDemon <vladlen2010@mail.ru>
2015-06-15 05:52:30 +0300

    Композиция

---
+++ Lesson29/Task1.java
@@ -0,0 +1,13 @@
+package Lesson29;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook();
+        notebook.on();
+        notebook.read();
+        notebook.ReadInfo();
+        notebook.SaveInfo();
+        notebook.writeDisk();
+        notebook.off();
+    }
+}
